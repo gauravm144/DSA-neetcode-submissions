57c3e38caee91e040eedcb086b90452ad7cd5059
Gaurav Manchanda <66792745+gauravm144@users.noreply.github.com>
2026-07-07 12:51:58 +0530

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> s=new HashSet<>();
+        while(n!=1){
+            int sum=get(n);
+            if(sum==1)
+            return true;
+            
+            if(s.contains(sum))
+            return false;
+
+            s.add(sum);
+            n=sum;
+        }
+        return true;
+    }
+    int get(int n){
+        int sum=0;
+        while(n!=0){
+            int val=n%10;
+            sum+=val*val;
+            n=n/10;
+        }
+        return sum;
+    }
+}
