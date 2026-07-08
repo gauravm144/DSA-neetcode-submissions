48e0ba85593bf1941327b6ea462a1c08a3380194
Gaurav Manchanda <66792745+gauravm144@users.noreply.github.com>
2026-07-08 08:23:23 +0530

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int carry=1;
+        for(int i=digits.length-1;i>=0;i--){
+            int sum=digits[i]+carry;
+            digits[i]=sum%10;
+            carry=sum/10;
+        }
+        if(carry!=0){
+            int[] ans=new int[digits.length+1];
+            for(int i=1;i<digits.length;i++){
+                ans[i]=digits[i-1];
+            }
+            ans[0]=carry;
+            digits=ans;
+        }
+        return digits;
+    }
+}
