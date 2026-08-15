8ab93c564de4fc1da59efd44a1251e9d18470815
Gaurav Manchanda <66792745+gauravm144@users.noreply.github.com>
2026-08-15 14:14:20 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.java
@@ -0,0 +1,53 @@
+class Trie {
+    Trie[] trie = new Trie[26];
+    boolean end = false;
+}
+class PrefixTree {
+    Trie root;
+    public PrefixTree() {
+        root = new Trie();
+    }
+
+    public void insert(String word) {
+        Trie tmp = root;
+        int idx = 0;
+        while (idx < word.length()) {
+            int pos=word.charAt(idx)-'a';
+            if (tmp.trie[pos] == null) {
+                tmp.trie[pos] = new Trie();
+                tmp = tmp.trie[pos];
+            } else
+                tmp = tmp.trie[pos];
+            idx++;
+        }
+        tmp.end = true;
+    }
+
+    public boolean search(String word) {
+        Trie tmp = root;
+        int idx = 0;
+        while (tmp != null && idx < word.length()) {
+             int pos=word.charAt(idx)-'a';
+            if (tmp.trie[pos] == null) {
+                return false;
+            } else
+                tmp = tmp.trie[pos];
+            idx++;
+        }
+        return tmp.end;
+    }
+
+    public boolean startsWith(String prefix) {
+        Trie tmp = root;
+        int idx = 0;
+        while (tmp != null && idx < prefix.length()) {
+             int pos=prefix.charAt(idx)-'a';
+            if (tmp.trie[pos] == null) {
+                return false;
+            } else
+                tmp = tmp.trie[pos];
+            idx++;
+        }
+        return true;
+    }
+}
